b326d9666dacfa63c6ca4119c0216c6aa159a54b
Jeremy Yang <jeremyjyang@gmail.com>
2022-04-18 10:26:35 -0600

    Ok.

--- sql/search_similarity.sql
+++ sql/search_similarity.sql
@@ -1,13 +1,17 @@
 SELECT
 	mols.id,
 	mols.name,
-	lincs.pert_name,
+	drugcentral.id AS dc_id,
+	drugcentral.name AS drugname,
+	lincs.pert_name AS lincs_name,
 	lincs.sig_count,
 	refmet.pubchem_cid,
 	refmet.refmet_name,
 	ROUND(tanimoto_sml(rdkit_fp(mol_from_smiles('NCCc1ccc(O)c(O)c1'::cstring)), mols.fp)::NUMERIC, 2) similarity
 FROM
 	mols
+JOIN
+        drugcentral ON (drugcentral.mol_id = mols.id)
 JOIN
 	lincs ON (lincs.mol_id = mols.id)
 JOIN
